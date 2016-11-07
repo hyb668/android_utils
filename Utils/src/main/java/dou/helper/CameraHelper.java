01828d7bf908df971b775dd8ba076584d6435eac
dou <hongbin.dou@gmail.com>
2016-11-07 12:29:40 +0800

    open camera

--- Utils/src/main/java/dou/helper/CameraHelper.java
+++ Utils/src/main/java/dou/helper/CameraHelper.java
@@ -66,6 +66,8 @@ public class CameraHelper implements PreviewCallback {
                     stopCamera();
                 }
             });
+        } else {
+            openCamera();
         }
     }
 
